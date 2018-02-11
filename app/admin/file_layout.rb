3fc8c3ee6296dde5f51de2098e45b742d7a9b940
Raghu Betina <raghu@firstdraft.com>
2018-02-11 04:41:12 +0000

    Whitelist columns in FileLayout for ActiveAdmin

--- app/admin/file_layout.rb
+++ app/admin/file_layout.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register FileLayout do
 
+ permit_params :file_name, :file_path
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
