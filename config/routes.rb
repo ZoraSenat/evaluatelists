d60e366b156fbcda51cadb8cd382334d3db866b5
Raghu Betina <raghu@firstdraft.com>
2018-02-11 04:40:47 +0000

    run rails g active_admin:install

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   root :to => "data_cards#index"
   # Routes for the Rfq resource:
   # CREATE
