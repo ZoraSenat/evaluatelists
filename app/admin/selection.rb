45e0b8b91781cc03a326365d12c272de5065e0ea
Raghu Betina <raghu@firstdraft.com>
2018-02-11 04:41:19 +0000

    Whitelist columns in Selection for ActiveAdmin

--- app/admin/selection.rb
+++ app/admin/selection.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Selection do
 
+ permit_params :select_id, :data_card_id
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
